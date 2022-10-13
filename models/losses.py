90bb089a02ae55a70c3f77bb815cd106c1cf579e
Cayman <56817261+Hugengyuan@users.noreply.github.com>
2022-10-13 17:04:48 +0800

    Update losses.py

--- models/losses.py
+++ models/losses.py
@@ -4,6 +4,7 @@ from utils.bbox_transforms import *
 from utils.box_coder import BoxCoder
 from utils.rotation_overlaps.rbox_overlaps import rbox_overlaps
 import matplotlib.pyplot as plt
+import os
 root_path = os.path.abspath(__file__)
 root_path = '/'.join(root_path.split('/')[:-2])
 sys.path.append(root_path)
